8a182818e141bb245f7855d3c7eac2b79eb67fcf
VoVanDuc20110635 <20110635@student.hcmute.edu.vn>
2024-07-05 07:45:15 +0700

    update time vnpay

--- src/main/java/com/data/filtro/service/VNPayService.java
+++ src/main/java/com/data/filtro/service/VNPayService.java
@@ -181,7 +181,7 @@ public class VNPayService {
 //    }
 
     private String vnpRequest(Order order, HttpServletRequest req){
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+8"));
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
         String vnp_CreateDate = dateFormat.format(calendar.getTime());
         calendar.add(Calendar.MINUTE, 15);
